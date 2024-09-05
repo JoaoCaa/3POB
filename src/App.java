521dcd8d9f5d3135d27b5f62ca523647b06f7a39
João Caamano <joaocaamano2003@gmail.com>
2024-09-05 10:47:01 -0300

    ajustes

--- src/App.java
+++ src/App.java
@@ -4,28 +4,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite a matricula: ");
-        String matrícula = scanner.nextLine();
+        System.out.print("Digite a temperatura em Farenheit: ");
+        double farenheit = scanner.nextDouble();
 
-        System.out.print("Digite o salario: ");
-        String salario = scanner.nextLine();
+        double celsius = 5.0/9.0 * (farenheit - 32);
 
-        System.out.print("Digite a matricula do segundo: ");
-        String matrícula2 = scanner.nextLine();
-
-        System.out.print("Digite o salario do segundo: ");
-        String salario2 = scanner.nextLine();
-
-        double desconto = Double.parseDouble(salario) * 0.05;
-        double acrescimo = Double.parseDouble(salario2) * 0.09;
-
-        double salarioFinal1 = Double.parseDouble(salario) - desconto;
-        double salarioFinal2 = Double.parseDouble(salario2) + acrescimo;
-
-        System.out.println("o funcionario " + matrícula + " tem o desconto: " + desconto);
-        System.out.println("Salário final do primeiro: " + salarioFinal1);
-        
-        System.out.println("o segundo funcionario " + matrícula2 + " tem o acrescimo: " + acrescimo);
-        System.out.println("Salário final do segundo: " + salarioFinal2);
+        System.out.println("A temperatura em Celsius é: " + celsius);
+ 
     }
 }
