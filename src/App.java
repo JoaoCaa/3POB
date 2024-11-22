6c9f9911b7114b9c987989813512ff3d10bfc2a8
João Caamano <joaocaamano2003@gmail.com>
2024-11-21 22:22:40 -0300

    ajustes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! 2");
+        Map<String, Integer> sexo = new HashMap<>();
+        sexo.put("M", 0);
+        sexo.put("F", 0);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o sexo ('F' para feminino ou 'M' para masculino), ou 'S' para sair: ");
+        String linha = scanner.nextLine();
+        while (!linha.equalsIgnoreCase("S")) {
+            sexo.put(linha, sexo.get(linha) + 1);
+            System.out.println("Digite o sexo ('F' para feminino ou 'M' para masculino), ou 'S' para sair: ");
+            linha = scanner.nextLine();
+        }
+        System.out.println("Total de pessoas cadastradas: " + (sexo.get("M") + sexo.get("F")));
+        System.out.println("Total de pessoas de sexo M: " + sexo.get("M"));
+        System.out.println("Total de pessoas de sexo F: " + sexo.get("F"));
     }
 }
+
