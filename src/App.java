eb46554d47abfd580dd5f518f0a47a8280496680
João Caamano <joaocaamano2003@gmail.com>
2024-11-21 22:21:04 -0300

    ajustes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! 2");
+        Scanner scanner = new Scanner(System.in);
+
+        double soma = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digite a idade da " + (i + 1) + " pessoa: ");
+            soma += scanner.nextInt();
+        }
+
+        double media = soma / 10;
+        System.out.println("M dia das idades: " + media);
     }
 }
+
